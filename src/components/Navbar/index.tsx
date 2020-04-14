e87506e7ebecb2c0f4b47cedb9498385703a76f0
Dhika Rizky <deka_r@outlook.com>
2020-04-15 00:58:37 +0800

    Added new navbar component

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import styled, { css, FlattenSimpleInterpolation } from 'styled-components'
+import Link from 'next/link'
+
+const Name = styled.h1`
+  font-family: Inter, -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Oxygen, Ubuntu, Cantarell, \'Open Sans\', \'Helvetica Neue\', sans-serif;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 48px;
+  color: #2d2d2d;
+  margin: 0px;
+`
+
+const ChineseName = styled.span`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  display: block;
+  color: #636363;
+`
+
+const Links = styled.ul`
+  list-style-type: none;
+  padding: 0px;
+  margin: 0px;
+  margin-top: 16px;
+  display: flex;
+`
+
+interface StyledLinkProps {
+  active?: boolean;
+}
+
+const StyledLink = styled.li<StyledLinkProps>`
+  flex: none;
+  order: 1;
+  align-self: flex-start;
+  margin: 0px 8px;
+
+  &:first-child {
+    margin-left: 0px;
+  }
+
+  &:last-child {
+    margin-right: 0px;
+  }
+
+  a {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    color: #636363;
+    text-decoration: none;
+    min-width: 43px;
+    display: inline-block;
+
+    &:hover {
+      color: #2d2d2d;
+    }
+  }
+
+  ${(props): FlattenSimpleInterpolation => props.active && css`
+    a {
+      color: #2d2d2d;
+      font-weight: 500;
+    }
+  `}
+`
+
+const Navbar = (): React.ReactElement => {
+  return (
+    <nav>
+      <Name>Dhika Rizky</Name>
+      <ChineseName>古育琦</ChineseName>
+      <Links>
+        <StyledLink>
+          <Link href="/" passHref>
+            <a>Home</a>
+          </Link>
+        </StyledLink>
+        <StyledLink>
+          <Link href="/notes" passHref>
+            <a>Notes</a>
+          </Link>
+        </StyledLink>
+        <StyledLink>
+          <Link href="/works" passHref>
+            <a>Works</a>
+          </Link>
+        </StyledLink>
+        <StyledLink>
+          <Link href="/stuffs" passHref>
+            <a>Stuffs</a>
+          </Link>
+        </StyledLink>
+        <StyledLink>
+          <Link href="/about" passHref>
+            <a>About</a>
+          </Link>
+        </StyledLink>
+      </Links>
+    </nav>
+  )
+}
+
+export default Navbar

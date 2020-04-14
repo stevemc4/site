dd40a9ab1dbb103f3cf130a4f60802d1d7198212
Dhika Rizky <deka_r@outlook.com>
2020-04-15 01:17:43 +0800

    Updated the navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,10 @@
 import React from 'react'
 
-import { Name, Work, WorkPlace, WorkPosition, Social } from '../styles/style'
+import Navbar from '../components/Navbar'
 
 const Index = (): React.ReactElement => (
   <>
-    <Name>
-      Dhika Rizky
-      <span className="chinese">古育琦</span>
-    </Name>
-    <Work>
-      <WorkPosition>Lead Frontend Engineer</WorkPosition>
-      <WorkPlace>at Kyou Hobby Shop</WorkPlace>
-    </Work>
-    <Social>
-      <li>
-        <a href="mailto:dhika@dhikarizky.me">mail</a>
-      </li>
-      <li>
-        <a href="https://linkedin.com/in/stevemc4" rel="noopener noreferrer" target="_blank">linkedin</a>
-      </li>
-      <li>
-        <a href="https://github.com/stevemc4" rel="noopener noreferrer" target="_blank">github</a>
-      </li>
-    </Social>
+    <Navbar />
   </>
 )
 

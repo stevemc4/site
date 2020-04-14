ad8a49a7bb103f93d6fca7b6e42d797d5d750620
Dhika Rizky <deka_r@outlook.com>
2020-04-15 01:03:31 +0800

    Added Inter font import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,7 +40,7 @@ class MyDocument extends Document {
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Overpass:400,700|Staatliches&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
           <Main />

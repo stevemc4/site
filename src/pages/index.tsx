9096e89c7cf2bc1253d7ec7eb45868b430adaa50
Dhika Rizky <deka_r@outlook.com>
2020-04-15 01:31:27 +0800

    Created basic index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Navbar from '../components/Navbar'
+
+const Greeting = styled.p`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 22px;
+
+  color: #636363;
+`
 
 const Index = (): React.ReactElement => (
   <>
     <Navbar />
+    <Greeting>
+      Hi There!<br />
+      I’m Dhika, a frontend engineer and a big weeb
+    </Greeting>
   </>
 )
 

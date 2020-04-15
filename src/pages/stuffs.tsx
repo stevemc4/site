3a41f08e732d73b6844377fb6513492267e39b76
Dhika Rizky <deka_r@outlook.com>
2020-04-16 00:22:22 +0800

    Integrating Prismic with Stuffs page

--- src/pages/stuffs.tsx
+++ src/pages/stuffs.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Predicates } from 'prismic-javascript'
+import PrismicDOM from 'prismic-dom'
+import ApiSearchResponse from 'prismic-javascript/d.ts/ApiSearchResponse'
+
+import Prismic from '../util/prismic'
 
 import Layout from '../layout/Default'
 
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/PageTitle'
+import List, { ListItem } from '../components/List'
 
 const Subtitle = styled.span`
   font-weight: normal;
@@ -15,7 +21,11 @@ const Subtitle = styled.span`
   display: block;
 `
 
-const Index = (): React.ReactElement => (
+interface StuffsProps {
+  response?: ApiSearchResponse;
+}
+
+const Stuffs = ({ response }: StuffsProps): React.ReactElement => (
   <Layout>
     <Navbar />
     <section>
@@ -26,7 +36,35 @@ const Index = (): React.ReactElement => (
         a.k.a Things I love, recommend, or just something I want to share
       </Subtitle>
     </section>
+    <List>
+      {response.results.map(item => {
+        const title = PrismicDOM.RichText.asText(item.data.title)
+        const primarySubtitle = PrismicDOM.RichText.asText(item.data.primarysubtitle)
+        const secondarySubtitle = PrismicDOM.RichText.asText(item.data.secondarysubtitle)
+        const description = PrismicDOM.RichText.asText(item.data.comment)
+        return (
+          <ListItem
+            title={title}
+            subtitle={`${primarySubtitle}${secondarySubtitle !== '' ? ` · ${secondarySubtitle}` : ''}`}
+            description={description}
+            key={item.uid}
+          />
+        )
+      })}
+    </List>
   </Layout>
 )
 
-export default Index
+export async function getStaticProps (): Promise<{props: StuffsProps}> {
+  const response = await Prismic().query(
+    Predicates.at('document.type', 'stuffs'),
+    { orderings: '[my.blog_post.date desc]' }
+  )
+  return {
+    props: {
+      response: response
+    }
+  }
+}
+
+export default Stuffs

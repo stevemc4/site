6ef138f51921a24a9fb84ef829f887fb4af9c7b8
Dhika Rizky <deka_r@outlook.com>
2020-04-16 23:36:25 +0800

    Added basic about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import styled from 'styled-components'
+import PrismicDOM from 'prismic-dom'
+import { Document } from 'prismic-javascript/d.ts/documents'
+
+import Prismic from '../util/prismic'
+
+import Layout from '../layout/Default'
+
+import Navbar from '../components/Navbar'
+import PageTitle from '../components/PageTitle'
+import TwoColumnList from '../components/TwoColumnList'
+import { ListItem } from '../components/List'
+
+const Description = styled.p`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 22px;
+
+  color: #636363;
+`
+
+interface AboutProps {
+  document: Document;
+}
+
+const About = ({ document }: AboutProps): React.ReactElement => (
+  <Layout>
+    <Navbar />
+    <PageTitle>
+      About Me
+    </PageTitle>
+    <Description>
+      { PrismicDOM.RichText.asText(document.data.description)}
+    </Description>
+    <TwoColumnList title="Education">
+      {document.data.education.map((edu): React.ReactElement => {
+        const name = PrismicDOM.RichText.asText(edu.name)
+        const major = PrismicDOM.RichText.asText(edu.major)
+        const year = PrismicDOM.RichText.asText(edu.year)
+
+        return (
+          <ListItem
+            title={name}
+            subtitle={`${major} · ${year}`}
+            key={name + ` [${year}]`}
+          />
+        )
+      })}
+    </TwoColumnList>
+    <TwoColumnList title="Working Experience">
+      {document.data.experience.map((exp): React.ReactElement => {
+        const name = PrismicDOM.RichText.asText(exp.company)
+        const role = PrismicDOM.RichText.asText(exp.role)
+        const year = PrismicDOM.RichText.asText(exp.year)
+
+        return (
+          <ListItem
+            title={name}
+            subtitle={`${role} · ${year}`}
+            key={name + ` [${year}]`}
+          />
+        )
+      })}
+    </TwoColumnList>
+  </Layout>
+)
+
+export default About
+
+export async function getStaticProps (): Promise<{ props: AboutProps }> {
+  const aboutPage = await Prismic().getSingle('about', {})
+  return {
+    props: {
+      document: aboutPage
+    }
+  }
+}

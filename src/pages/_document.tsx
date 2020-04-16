db9150bbbe54981baa3b0a1dcf7abc590b9876bf
Dhika Rizky <deka_r@outlook.com>
2020-04-16 22:03:22 +0800

    Added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,7 @@ class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet"></link>
+          <link rel="shortcut icon" href="/favicon.png" />
         </Head>
         <body>
           <Main />

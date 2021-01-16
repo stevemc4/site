dbb611e9248e7d9f4e1559e1efbce09b8bcb0e4e
Dhika Rizky <deka_r@outlook.com>
2021-01-16 20:46:08 +0700

    Moved tags in _document to _app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,13 +36,7 @@ class MyDocument extends Document {
   render (): React.ReactElement {
     return (
       <Html lang="en">
-        <Head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet"></link>
-          <link rel="shortcut icon" href="/favicon.png" />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />

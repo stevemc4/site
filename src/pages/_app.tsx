c9e01d91426c0d015e5d4459e0fa93cc24841686
Dhika Rizky <deka_r@outlook.com>
2021-11-08 20:49:08 +0700

    Updated Inter font import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import { createGlobalStyle } from 'styled-components'
 import 'normalize.css'
 
 const Global = createGlobalStyle`
-
   body {
     background: #ffffff;
     color: #2d2d2d;
@@ -16,6 +15,10 @@ const Global = createGlobalStyle`
     @media (prefers-color-scheme: dark) {
       background: #2d2d2d;
       color: #ebebeb;
+    }
+
+    @supports (font-variation-settings: normal) {
+      font-family: "Inter var", -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Oxygen, Ubuntu, Cantarell, \'Open Sans\', \'Helvetica Neue\', sans-serif;
     }
   }
 `
@@ -28,7 +31,7 @@ function App ({ Component, pageProps }: {Component: NextComponentType; pageProps
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet"></link>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <link rel="shortcut icon" href="/favicon.png" />
         <title>Dhika Rizky</title>
         <meta name="description" content="Hi There! I’m Dhika Rizky, a frontend engineer and a big weeb" />

00a6a2196e4de4601d23c26c435a55a72244d211
Dhika Rizky <deka_r@outlook.com>
2022-01-30 23:03:13 +0700

    Migrated homepage to astro

--- src/components/Navbar/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Navbar from '.'
-
-export default {
-  title: 'Components/Navbar',
-  component: Navbar
-}
-
-export const Default = (): React.ReactElement => <Navbar />

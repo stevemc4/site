7be8737e01775c1a558e8615fdbf7351c0dcc75f
Dhika Rizky <deka_r@outlook.com>
2026-01-23 15:44:41 +0700

    Remove prismic

--- src/util/prismic.ts
+++
@@ -1,6 +0,0 @@
-import Prismic from 'prismic-javascript'
-import { DefaultClient } from 'prismic-javascript/types/client'
-
-const PrismicApi = (): DefaultClient => Prismic.client('https://dhikarizky.cdn.prismic.io/api/v2')
-
-export default PrismicApi

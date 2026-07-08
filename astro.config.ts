faef6e7481e6a37fa54a21b2256439a7dbf31725
Dhika Rizky <deka_r@outlook.com>
2026-07-08 21:20:44 +0700

    Ported site to plain HTML (#47)
    * Ported site to plain HTML
    
    * Change package manager to pnpm

--- astro.config.ts
+++
@@ -1,5 +0,0 @@
-export default ({
-  buildOptions: {
-    site: 'https://dhikarizky.me'
-  }
-})
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      colors: {
+        ink: '#2d2d2d',
+        'ink-dark': '#ebebeb',
+        muted: '#636363',
+        'muted-dark': '#c4c4c4',
+        paper: '#ffffff'
+      },
+      fontFamily: {
+        sans: [
+          'Inter var',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          'Open Sans',
+          'Helvetica Neue',
+          'sans-serif'
+        ]
+      }
+    }
+  },
+  plugins: []
+}
